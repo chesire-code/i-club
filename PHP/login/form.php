ccf0ace16849879c24a95077f4d7847c01eef883
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2021-07-01 19:09:39 +0430

    Update form.php

--- PHP/login/form.php
+++ PHP/login/form.php
@@ -9,3 +9,23 @@
 PARAMETERS : user_ID, email address, password
 */
 
+/*
+
+($_SERVER["REQUEST_METHOD"] == "POST") ? Run program : Terminate Session ;
+
+$is_admin = ($user['permissions'] == 'admin') ? true : false;
+
+echo 'Welcome '.($user['is_logged_in'] ? $user['first_name'] : 'Guest').'!';
+
+echo 'Your cart contains '.$num_items.' item'.($num_items != 1 ? 's' : '').'.';
+
+error_reporting($WEBSITE_IS_LIVE ? 0 : E_STRICT);
+
+echo '<base href="http'.($PAGE_IS_SECURE ? 's' : '').'://mydomain.com" />';
+
+echo 'Your score is:  '.($score > 10 ? ($age > 10 ? 'Average' : 'Exceptional') : ($age > 10 ? 'Horrible' : 'Average') )
+
+$is_leap_year = ((($year % 4) == 0) && ((($year % 100) != 0) || (($year %400) == 0)));
+
+header('Location: '.($valid_login ? '/members/index.php' : 'login.php?errors=1')); exit();
+*/
